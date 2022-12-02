988d5f13dfa5c52d79ffc2e31d52c3c5da8875b1
CaoThang <tcao2315@gmail.com>
2022-12-02 11:51:00 +0700

    ok

---
+++ src/Screens/TuyChonTrangCaNhan/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import Icon2 from "@expo/vector-icons/AntDesign";
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Avatar, Input } from 'react-native-elements';
+import { theme } from "../../Mau/theme";
+
+import { Api } from "../../Global/Axios/Api";
+const TuyChonTrangCaNhan = ({route}) => {
+    const navigation = useNavigation()
+    return (
+        <View style={styles.container}>
+            <View style={{ height: 70, backgroundColor: 'blue' }}>
+                <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                    <TouchableOpacity >
+                        <Icon2 style={{ marginTop: 25, marginLeft: 20 }}
+                            name="left"
+                            size={25}
+                            color={theme.colors.white}
+                        />
+                    </TouchableOpacity>
+                    <Text style={{ marginTop: 25, marginLeft: 15, fontSize: 17, fontWeight: 'bold', color: 'white' }}>Hoàng Tiến Mạnh Đức</Text>
+                </View>
+            </View>
+            <View style={{marginTop:10,marginLeft:20}}>
+                <TouchableOpacity onPress={() => navigation.navigate("ThongTinCaNhan",{user:route.params.user})}>
+                    <Text style={{fontSize:17}}>Thông tin</Text>
+                </TouchableOpacity>
+                <View style={{ height: 1, backgroundColor: '#e6e6fa', marginTop: 10 }}></View>
+                <TouchableOpacity style={{marginTop:10}}>
+                    <Text style={{fontSize:17}}>Cập nhật hình đại diện</Text>
+                </TouchableOpacity>
+                <View style={{ height: 1, backgroundColor: '#e6e6fa', marginTop: 10 }}></View>
+                <TouchableOpacity style={{marginTop:10}}>
+                    <Text style={{fontSize:17}}>Cập nhật ảnh bìa</Text>
+                </TouchableOpacity>
+                <View style={{ height: 1, backgroundColor: '#e6e6fa', marginTop: 10 }}></View>
+                <TouchableOpacity style={{marginTop:10}}>
+                    <Text style={{fontSize:17}}>Cập nhật giới thiệu bản thân</Text>
+                </TouchableOpacity>
+                <View style={{ height: 2, backgroundColor: '#e6e6fa', marginTop: 10 }}></View>
+                <TouchableOpacity style={{marginTop:10}}>
+                    <Text style={{fontSize:17}}>Ví của tôi</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    }
+});
+
+export default TuyChonTrangCaNhan;
